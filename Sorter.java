cfa060c7273976d132efb76e397f01e312b40dc7
Mohammed <m7med2003gh@gmail.com>
2023-02-07 20:11:59 +0300

    Added selection sort method and fixed the generation of the random array.

--- Sorter.java
+++ Sorter.java
@@ -1,4 +1,3 @@
-import java.util.random.RandomGenerator;
 import java.util.Random;
 public class Sorter {
 
@@ -6,15 +5,38 @@ public class Sorter {
         System.out.println("Everything is fine.\n Initializing an array of 10 random integers to be sorted.");
         int[] integers = new int[10];
         Random r = new Random();
-        for(int i = 0; i < 9; i++) {
+        for(int i = 0; i < 10; i++) {
             integers[i] = r.nextInt(20);
         }
         System.out.println("The unsorted array is: ");
-        for(int i = 0; i < 9; i++) {
+        for(int i = 0; i < 10; i++) {
             System.out.print(integers[i]+" ");
-            
         }
+        System.out.println("\nSorted array:");
+        int[] sortedIntegers = selectionSort(integers);
+        for(int i = 0; i < sortedIntegers.length; i++) {
+            System.out.print(sortedIntegers[i] + " ");
+        }
+        
+    }
 
+    // Method that returns the given array sorted in ascending order.
+    public static int[] selectionSort(int[] arr) {
+        int[] sortedArr = arr.clone();
+        int indexOfMin;
+        int temp;
+        for(int i = 0; i < arr.length; i++) {
+            indexOfMin = i;
+            for(int j = i+1; j<sortedArr.length; j++) {
+                if(sortedArr[j] < sortedArr[indexOfMin]) {
+                    indexOfMin = j;
+                }
+            }
+            temp = sortedArr[i];
+            sortedArr[i] = sortedArr[indexOfMin];
+            sortedArr[indexOfMin] = temp;
+        }
+        return sortedArr;
     }
 
 }
